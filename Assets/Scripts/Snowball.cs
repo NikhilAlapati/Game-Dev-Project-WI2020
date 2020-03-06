c06fa62d4bd7cba2d6fa293449333834705f8c35
joshjke <joshjkennedy72@gmail.com>
2020-03-05 16:15:59 -0800

    added sounds, animation, and destruction for snowballs

--- Assets/Scripts/Snowball.cs
+++ Assets/Scripts/Snowball.cs
@@ -8,10 +8,55 @@ public class Snowball : MonoBehaviour
     public int damage = 1;
     public Player thrower;
     public Team team;
+    public Animator anim;
+    public AudioSource audioSource;
+
+    public AudioClip[] playerHitSounds;
+    public SoundPool playerHitSoundPool = new SoundPool();
+
+    public AudioClip[] wallHitSounds;
+    public SoundPool wallHitSoundPool = new SoundPool();
+
+    private bool alreadyHit = false;
 
     private void Start()
     {
-        Invoke("SelfDestruct", 10);
+        playerHitSoundPool.soundsInPool = playerHitSounds;
+        wallHitSoundPool.soundsInPool = wallHitSounds;
+
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        Invoke("Explode", 1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (alreadyHit)
+            return;
+
+        // hitting a player or another snowball
+        if (collision.gameObject.GetComponent<Player>() != null || collision.gameObject.GetComponent<Snowball>() != null)
+            audioSource.clip = playerHitSoundPool.GetRandomSound();
+        // hitting a wall
+        else if (collision.gameObject.CompareTag("wall"))
+            audioSource.clip = wallHitSoundPool.GetRandomSound();
+        // disregard
+        else
+        {
+            return;
+        }
+
+        alreadyHit = true;
+        audioSource.Play();
+        Explode();
+    }
+
+    private void Explode()
+    {
+        // play sound
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+        transform.Rotate(Vector3.forward, Random.Range(0, 360));
+        anim.SetTrigger("Explode");
     }
 
     private void SelfDestruct()
---
+++ Assets/SoundPool.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPool
+{
+    public AudioClip[] soundsInPool;
+    private int currentSoundIndex = 0;
+
+    public AudioClip GetRandomSound()
+    {
+        return GetRandomSound(0);
+    }
+
+    public AudioClip GetRandomSound(ulong delay)
+    {
+        if (soundsInPool[0] == null)
+        {
+            Debug.Log("sounds index 0 is == null");
+            return null;
+        }
+        else if (soundsInPool[soundsInPool.Length - 1] == null)
+        {
+            Debug.Log("sounds index " + (soundsInPool.Length - 1) + " is == null");
+            return null;
+        }
+
+        int randomClipIndex = Random.Range(0, soundsInPool.Length -1);
+        return GetSoundAtIndex(randomClipIndex, delay);
+    }
+
+    public AudioClip GetSoundInOrder()
+    {
+        return GetSoundInOrder(0);
+    }
+
+    public AudioClip GetSoundInOrder(ulong delay)
+    {
+        if (currentSoundIndex > soundsInPool.Length - 1)
+            currentSoundIndex = 0;
+        else
+            currentSoundIndex++;
+        return GetSoundAtIndex(currentSoundIndex, delay);
+
+    }
+    private AudioClip GetSoundAtIndex(int index)
+    {
+        return GetSoundAtIndex(index, 0);
+    }
+
+
+    private AudioClip GetSoundAtIndex(int index, ulong delay)
+    {
+        if (soundsInPool[index] == null)
+        {
+            Debug.Log("sounds index " + index + " is == null");
+            return null;
+        }
+
+        return soundsInPool[index];
+
+    }
+}
